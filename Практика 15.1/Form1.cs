add87b6711dd3d5f60d981146ea81c8b2320eb9f
Алина Крылатова <krylalina@gmail.com>
2020-05-12 16:05:47 +0300

    Добавьте файлы проекта.

---
+++ Практика 15.1/Form1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Практика_15._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "")
+                textBox2.Text = "Хорошего Вам дня, " + textBox1.Text;
+            else
+                textBox2.Text = "Хорошего дня, незнакомец";
+        }
+    }
+}
